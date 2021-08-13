26c591311baf1892635172c206cb28fa652d4f16
HLazo <heberlazo@gmail.com>
2021-08-13 12:49:34 -0500

    Add working sample

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React</h1>
+      <h2>Webpack</h2>
+      <h3>SASS</h3>
+    </div>
+  );
+}
+
+export default App;
