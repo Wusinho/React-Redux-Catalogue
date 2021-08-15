f850fbf1d23ccd7b0a8b4bee017d3d99091756e1
HLazo <heberlazo@gmail.com>
2021-08-15 16:47:45 -0500

    Corrected styling to cards

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,19 +1,33 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const getPicture = (champ) => `url("https:ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ}_0.jpg")`;
+
 const Book = ({ books }) => {
   const getBooks = Object.entries(books).map((item) => (item[1]));
 
   return getBooks.map((book) => (
-    <div className="card my-3" key={book.key}>
+    <div
+      className="card mb-3"
+      key={book.key}
+      style={{
+        backgroundImage: getPicture(book.name),
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <div className="card-body">
-        <h6 className="card-subtitle mb-2 text-muted">{book.title}</h6>
-        <h5 className="card-title">{book.name}</h5>
+        <h5 className="card-title">{book.title}</h5>
+        <h2 className="card-title">{book.name}</h2>
         <p className="card-text">{book.blurb}</p>
       </div>
     </div>
   ));
 };
+
+export default Book;
+
 Book.defaultProps = {
   book: {
     id: '',
@@ -26,4 +40,3 @@ Book.defaultProps = {
 Book.propTypes = {
   book: PropTypes.objectOf(Object),
 };
-export default Book;
