a371ee3f1ec11e824b8772277b6e709cdf7e570e
HLazo <heberlazo@gmail.com>
2021-08-15 15:52:39 -0500

    Corrected Book proptypes

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Book = ({ books }) => books.map((book) => (
-  <div className="card my-3" key={book.key}>
-    <div className="card-body">
-      <h6 className="card-subtitle mb-2 text-muted">{book.title}</h6>
-      <h5 className="card-title">{book.name}</h5>
-      <p className="card-text">{book.blurb}</p>
-    </div>
-  </div>
-));
+const Book = ({ books }) => {
+  const getBooks = Object.entries(books).map((item) => (item[1]));
 
+  return getBooks.map((book) => (
+    <div className="card my-3" key={book.key}>
+      <div className="card-body">
+        <h6 className="card-subtitle mb-2 text-muted">{book.title}</h6>
+        <h5 className="card-title">{book.name}</h5>
+        <p className="card-text">{book.blurb}</p>
+      </div>
+    </div>
+  ));
+};
 Book.defaultProps = {
   book: {
     id: '',
@@ -21,7 +24,6 @@ Book.defaultProps = {
 };
 
 Book.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  books: PropTypes.array,
+  book: PropTypes.objectOf(Object),
 };
 export default Book;
--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -4,10 +4,8 @@ import Book from './Book';
 import Loading from './Loading';
 
 const BooksList = () => {
-  const getBooks = useSelector((state) => state.entities.books.list);
+  const books = useSelector((state) => state.entities.books.list);
   const loadingState = useSelector((state) => state.entities.books.loading);
-
-  const books = Object.entries(getBooks).map((item) => (item[1]));
 
   return (
     loadingState ? <Loading /> : <Book books={books} />
