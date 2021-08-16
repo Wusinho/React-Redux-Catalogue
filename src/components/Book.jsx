21e5247f6eb7986d0f15b28c054850cfada37437
HLazo <heberlazo@gmail.com>
2021-08-16 10:25:53 -0500

    Corrected image request and added styling

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,23 +1,23 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const getPicture = (champ) => `url("https:ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ}_0.jpg")`;
-
 const Book = ({ books }) => {
   const getBooks = Object.entries(books).map((item) => (item[1]));
+  const getPicture = (name) => `url("https:ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg")`;
 
   return getBooks.map((book) => (
     <div
       className="card m-2 rounded-3 book__card"
       key={book.key}
-      style={{
-        backgroundImage: getPicture(book.name),
-      }}
     >
-      <div className="card-body">
+      <div
+        className="card-body"
+        style={{
+          backgroundImage: getPicture(book.id),
+        }}
+      >
         <h5 className="card-title">{book.title}</h5>
         <h2 className="card-title">{book.name}</h2>
-        {/* <p className="card-text">{book.blurb}</p> */}
       </div>
     </div>
   ));
