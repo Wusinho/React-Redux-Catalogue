3422e8ef9c5eb921cdd3e983b5d984d2074e3ab7
HLazo <heberlazo@gmail.com>
2021-08-16 12:58:00 -0500

    Corrected Items fetch

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Item = () => (
-  <div>
-    <h1>Item</h1>
-  </div>
-);
+const Item = ({ match }) => {
+  const [char, setChar] = useState({
+    image: {},
+  });
+
+  const fetchItem = async () => {
+    const data = await fetch(
+      `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion/${match.params.id}.json`,
+    );
+    const items = await data.json();
+    setChar(items.data);
+  };
+
+  useEffect(() => {
+    fetchItem();
+  }, []);
+
+  console.log(char);
+  return (
+    <div>
+      {/* <h1>{char.name}</h1>
+      <h1>{char.species}</h1>
+      <h1>{char.gender}</h1>
+      <img src={char.image} alt="" /> */}
+      <h1>champ</h1>
+    </div>
+  );
+};
 
 export default Item;
+
+Item.defaultProps = {
+  match: '',
+};
+
+Item.propTypes = {
+  match: PropTypes.string,
+};
