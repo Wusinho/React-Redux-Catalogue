5612069c2a6d462de69784fc614dd46f20a0e084
HLazo <heberlazo@gmail.com>
2021-08-15 22:34:26 -0500

    Corrected Navbar position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,21 +13,21 @@ const Navbar = () => {
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">Navbar</span>
+        <form>
+          <select
+            className="form-control"
+            onChange={(e) => handleFilterCHange(e)}
+          >
+            <option defaultValue disabled>Choose a Category</option>
+            <option key="0"> All </option>
+            {categories.map((cat) => (
+              <option key={cat.id} value={cat.cat}>
+                {cat.cat}
+              </option>
+            ))}
+          </select>
+        </form>
       </div>
-      <form>
-        <select
-          className="form-control"
-          onChange={(e) => handleFilterCHange(e)}
-        >
-          <option defaultValue disabled>Choose a Category</option>
-          <option key="0"> All </option>
-          {categories.map((cat) => (
-            <option key={cat.id} value={cat.cat}>
-              {cat.cat}
-            </option>
-          ))}
-        </select>
-      </form>
     </nav>
   );
 };
