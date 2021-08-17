4901a2d578e90905ebe20b0922552ce7f7516d8a
HLazo <heberlazo@gmail.com>
2021-08-17 14:36:40 -0500

    Bug with img request

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../style/Book.scss';
 
 const Book = ({ books }) => {
+  const [img, setImg] = useState('');
+  setImg('prubea');
+  console.log(img);
   const getBooks = Object.entries(books).map((item) => (item[1]));
-  const getPicture = (name) => `url("https:ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg")`;
+
+  const fetchImg = (name) => {
+    axios.get(`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg`)
+      .then((response) => { console.log(response); })
+      .catch((error) => { console.log(error.message); });
+  };
+
+  // const fetchImg = async (name) => {
+  //   const data = await fetch(
+  //     `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg`,
+  //   );
+  //   setImg(data.url);
+  // };
+  // console.log(img);
 
   return getBooks.map((book) => (
     <div
@@ -13,7 +30,7 @@ const Book = ({ books }) => {
       key={book.key}
       style={{
         minWidth: '300px',
-        backgroundImage: getPicture(book.id),
+        backgroundImage: fetchImg(book.id),
         backgroundColor: 'black',
       }}
     >
--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import * as actions from '../api';
 
+// eslint-disable-next-line consistent-return
 const api = ({ dispatch }) => (next) => (action) => {
   if (action.type !== actions.apiCallBegan.type) return next(action);
 
@@ -9,8 +10,8 @@ const api = ({ dispatch }) => (next) => (action) => {
   if (onStart) dispatch({ type: onStart });
   next(action);
 
-  return axios
-    .get('https://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion.json')
+  axios
+    .get('http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion.json')
     .then((response) => {
       dispatch(actions.apiCallSuccess(response.data.data));
       if (onSuccess) dispatch({ type: onSuccess, payload: response.data.data });
