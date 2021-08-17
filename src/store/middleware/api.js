65e1cd79ea4b852f3d85ef8000e4c4c3ca141147
HLazo <heberlazo@gmail.com>
2021-08-17 13:03:56 -0500

    Fixed linters

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,29 +1,24 @@
-/* eslint-disable */
-import axios from "axios";
-import * as actions from '../api'
+import axios from 'axios';
+import * as actions from '../api';
 
-const api =
-  ({ dispatch }) =>
-  (next) =>
-  (action) => {
-    if (action.type !== actions.apiCallBegan.type) return next(action);
+const api = ({ dispatch }) => (next) => (action) => {
+  if (action.type !== actions.apiCallBegan.type) return next(action);
 
-    const { url, method, data, onStart, onSuccess, onError } = action.payload;
+  const { onStart, onSuccess, onError } = action.payload;
 
-    if (onStart) dispatch({ type: onStart});
-    next(action);
+  if (onStart) dispatch({ type: onStart });
+  next(action);
 
-    axios
-      .get(
-      "http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion.json",
-      )
-      .then(response => {
-        dispatch(actions.apiCallSuccess(response.data.data));
-        if (onSuccess) dispatch({ type: onSuccess, payload: response.data.data })})
-      // })
-      .catch(error => {
-        dispatch(actions.apiCallFailed(error.message));
-        if (onError) dispatch({ type: onError, payload: error.message} )});
-  };
+  return axios
+    .get('http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion.json')
+    .then((response) => {
+      dispatch(actions.apiCallSuccess(response.data.data));
+      if (onSuccess) dispatch({ type: onSuccess, payload: response.data.data });
+    })
+    .catch((error) => {
+      dispatch(actions.apiCallFailed(error.message));
+      if (onError) dispatch({ type: onError, payload: error.message });
+    });
+};
 
 export default api;
