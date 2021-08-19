743152ca8bc9859e8bb4e6fb15cd7d0b4f7eaa67
HLazo <heberlazo@gmail.com>
2021-08-19 14:41:49 -0500

    Refactor Api call

--- src/__test__/components/SelectedChamp.test.js
+++ src/__test__/components/SelectedChamp.test.js
@@ -5,18 +5,17 @@ import '@testing-library/jest-dom/extend-expect';
 import SelectedChamp from '../../components/SelectedChamp';
 import configureStoreApp from '../../store/configureStore';
 
-const match = {
-  path: '/champs/:id',
-  url: '/champs/Ahri',
-  isExact: true,
-  params: {
-    id: 'Ahri',
-  },
-};
-
 const store = configureStoreApp();
 
 test('renders content', () => {
+  const match = {
+    path: '/champs/:id',
+    url: '/champs/Ahri',
+    isExact: true,
+    params: {
+      id: 'Ahri',
+    },
+  };
   const component = render(
     <Provider store={store}>
       <SelectedChamp match={match} />
@@ -24,3 +23,13 @@ test('renders content', () => {
   );
   expect(component);
 });
+
+// test('renders content', () => {
+//   const match = {};
+//   const component = render(
+//     <Provider store={store}>
+//       <SelectedChamp match={match} />
+//     </Provider>,
+//   );
+//   expect(component);
+// });
--- src/components/SelectedChamp.jsx
+++ src/components/SelectedChamp.jsx
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import Loading from './Loading';
 import Champ from './Champ';
 import champSelected from '../store/championaction';
 
 const SelectedChamp = ({ match }) => {
+  const ID = match.params.id;
   const [char, setChar] = useState({});
   const [img, setImg] = useState({});
   const dispatch = useDispatch();
 
   const fetchChamp = async () => {
-    const data = await fetch(
-      `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion/${match.params.id}.json`,
-    );
-    dispatch(champSelected(match.params.id));
-    const items = await data.json();
-    setChar(items.data);
+    axios.get(
+      `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion/${ID}.json`,
+    ).then((response) => {
+      const res = response.data;
+      setChar(res.data);
+    }).catch((err) => { console.error(err.message); });
+    dispatch(champSelected(ID));
   };
 
   const fetchImg = async () => {
     const data = await fetch(
-      `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${match.params.id}_0.jpg`,
+      `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${ID}_0.jpg`,
     );
     setImg(data);
   };
