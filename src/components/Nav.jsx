5a6ba4b1ea62eea66a8a9fd98b1c4d50ea90aa34
Heber Lazo <heberlazo@gmail.com>
2021-09-10 12:09:11 -0500

    Added Login and register in navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  // selectCurrentUser,
+  logOut,
+  selectIsLoggedIn,
+} from '../store/sessionSlice';
+
 import '../style/Nav.scss';
 
-const Nav = () => (
-  <nav className="sticky-top navbar">
-    <ul className="nav-links">
-      <Link to="/">
-        <li>Home</li>
-      </Link>
-      <Link to="/champs">
-        <li>Champs</li>
-      </Link>
-      <Link to="/registration">
-        <li>Registration</li>
-      </Link>
-      <Link to="/sign_in">
-        <li>Sign in</li>
-      </Link>
+const Nav = () => {
+  const loggedIn = useSelector(selectIsLoggedIn);
+  // const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
 
-    </ul>
-  </nav>
-);
+  return (
+    <nav className="sticky-top navbar">
+      <ul className="nav-links">
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/champs">
+          <li>Champs</li>
+        </Link>
+        {loggedIn ? (
+          <>
+            <button type="button" onClick={handleLogout} className="logout">Log Out</button>
+          </>
+        )
+          : (
+            <>
+              <Link to="/registration">
+                <li>Registration</li>
+              </Link>
+              <Link to="/sign_in">
+                <li>Sign in</li>
+              </Link>
+            </>
+          )}
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
