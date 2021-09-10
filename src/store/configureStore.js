686778e48a29e02f7b1070dab7799b9b457e4eb8
Heber Lazo <heberlazo@gmail.com>
2021-09-10 10:20:03 -0500

    Changed sigin middleware name

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
 import api from './middleware/api';
 import champApi from './middleware/champApi';
+import SignIn from './middleware/login';
 
 const store = configureStore({
   reducer,
   middleware: [
     api,
     champApi,
+    SignIn,
   ],
 });
 
