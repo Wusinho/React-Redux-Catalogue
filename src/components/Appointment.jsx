da62b545622887d31c417f25d42c26436cd14088
Heber Lazo <heberlazo@gmail.com>
2021-09-10 21:37:52 -0500

    Refactor Appointment component

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import Loading from './Loading';
 import {
   setAppointments,
   selectCurrentUserID,
   selectCurrentUserToken,
+  loadcoach,
+  selectCoachList,
 } from '../store/sessionSlice';
 
-const AppointmentForm = ({ coaches }) => {
+const AppointmentForm = () => {
   const dispatch = useDispatch();
 
   const currentUserID = useSelector(selectCurrentUserID);
   const currentUserToken = useSelector(selectCurrentUserToken);
+  const coaches = useSelector(selectCoachList);
+
   const [app, setApp] = useState({
     user_id: currentUserID,
     coach_id: '',
@@ -30,39 +35,37 @@ const AppointmentForm = ({ coaches }) => {
     });
   };
 
+  useEffect(() => {
+    dispatch(loadcoach(currentUserToken));
+  }, []);
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <select
-          className="form-control"
-          onChange={handleChange}
-          name="coach_id"
-        >
-          <option key="0" disabled>Choose</option>
-          {coaches.map((cat) => (
-            <option key={cat.id} value={cat.id}>
-              {cat.country}
+      {coaches && currentUserToken ? (
+        <form onSubmit={handleSubmit}>
+          <select
+            className="form-control"
+            onChange={handleChange}
+            name="coach_id"
+          >
+            <option key="0" disabled>
+              Choose
             </option>
-          ))}
-        </select>
-        {/* <input type="text" name='country' placeholder="country" onChange={handleChange}/> */}
-        <input type="date" name="date" onChange={handleChange} />
-        <button type="submit">Add booking</button>
-      </form>
+            {coaches.map((cat) => (
+              <option key={cat.id} value={cat.id}>
+                {cat.country}
+              </option>
+            ))}
+          </select>
+          {/* <input type="text" name='country' placeholder="country" onChange={handleChange}/> */}
+          <input type="date" name="date" onChange={handleChange} />
+          <button type="submit">Add booking</button>
+        </form>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
 
 export default AppointmentForm;
-
-AppointmentForm.defaultProps = {
-  coaches: {
-    key: '',
-    country: '',
-    name: '',
-  },
-};
-
-AppointmentForm.propTypes = {
-  coaches: PropTypes.objectOf(Object),
-};
