2b754f6a10f40425341854de71cd7e891938718b
Heber Lazo <heberlazo@gmail.com>
2021-09-11 11:49:36 -0500

    Improved appointmets card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 
 function Card({ card }) {
   return card.map((appointment) => (
-    <div
-      key={appointment.id}
-    >
-      <div>
-        App
-        <h4>{appointment.date}</h4>
-        <h6>{appointment.coach_id}</h6>
+    <div key={appointment.id} className="card">
+      <div className="card-body">
+        <h5 className="card-title">{appointment.user.username}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{appointment.date}</h6>
+        <p className="card-text">{appointment.coach.name}</p>
       </div>
-
     </div>
   ));
 }
