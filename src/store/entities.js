3964905a55079580eb97e5bedc2a8d6423e7da11
Heber Lazo <heberlazo@gmail.com>
2021-09-14 00:11:05 -0500

    Initial dist subtree commit

--- src/store/entities.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import champReducer from './books';
-import sessionReducer from './sessionSlice';
-
-export default combineReducers({
-  champs: champReducer,
-  session: sessionReducer,
-});
