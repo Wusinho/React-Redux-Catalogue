aa5606276a7eeaa647cf66e08ea2d361d3ff1a04
Heber Lazo <heberlazo@gmail.com>
2021-09-29 10:56:41 -0500

    Removed unnecessary code

--- src/store/middleware/registration.js
+++ src/store/middleware/registration.js
@@ -30,7 +30,6 @@ const registration = ({ dispatch }) => (next) => (action) => {
     .then((response) => {
       dispatch(actions.regCallSuccess(response));
       if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
-      console.log(response.data);
     })
     .catch((error) => {
       dispatch(actions.regCallFailed(error.message));
